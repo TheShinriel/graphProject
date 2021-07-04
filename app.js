1a3d7128d4a704dffa2c0da708218d481c9008ab
Pierre Henri <payhache@gmail.com>
2021-07-04 16:10:23 +0200

    ajout de la div agreement

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
 
 let submitBtn = document.querySelector('.calc_data');
+let divAgreement = document.querySelector('.agreement');
+let divCalcTox = document.querySelector('.calc_tox') 
+let checkBoxAgreement = document.querySelector('input[id="accept_agreement"]');
 let ingestionTimeInput = document.querySelector('.time_after_ingestion');
 let paracetamolConcentrationInput = document.querySelector('.paracetamol_concentration');
-
 let timeAfterIngestion = 15;
 let paracetamolConcentration = 180;
+
+console.log(divCalcTox);
 
 let context = document.querySelector('.graph');
 
 
 
 let graph = new Chart(context, {
-    data: {
-        datasets: [{
+    data: 
+    {
+        datasets: 
+        [
+            {
             type: 'line',
             label: 'Ligne de tox',
             data: [,200,100,50,25,12.5,6.25]
-        }, {
+            },
+
+            {
             type: 'line',
             label: 'Ligne de traitement',
             data: [,150,75,37.5,18.75,9.375,4.6875],
-        }, {
+            },
+
+            {
             type: 'bubble',
             label: 'calcul',
-            data: [{
-            }]
-        }
-    ],
+            data: [{}]
+            }
+        ],
         labels: [0,4,8,12,16,20,24]
-    
     },
     options: {
         responsive: true,
@@ -61,6 +70,12 @@ submitBtn.addEventListener("click", () => {
     addData(graph, timeAfterIngestion, paracetamolConcentration )
 })
 
+checkBoxAgreement.addEventListener("click", () => {
+    if (checkBoxAgreement.value) {
+        divAgreement.setAttribute("class", "invisible");
+        divCalcTox.setAttribute("class", "visible");
+    }
+})
 
 function addData(chart, time, concentration) {
     chart.data.datasets[2].data[0]= {x:time,y:concentration,r:10};
