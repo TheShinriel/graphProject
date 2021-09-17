98d9ff03747b134279e6b959dc5d6d284450e337
Pierre Henri <payhache@gmail.com>
2021-09-17 21:07:49 +0200

    facto du menu en cours

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,30 @@
-window.addEventListener("DOMContentLoaded", (event) => {
+import { hideHtmlElement, showHtmlElement } from "./utils/Dom";
+
+window.addEventListener("DOMContentLoaded", () => {
     const menu = document.getElementById("myNav");
     const btnBurger = document.getElementById("btn-burger");
+
     btnBurger.addEventListener('click',function(){
         menu.style.display = (menu.style.display == 'none')? '':'none';
     });
+
     window.onload = function(){
-        let ww = window.innerWidth;
-        menu.style.display = ( ww > 960 ) ? '':'none';
-        btnBurger.style.display = ( ww > 960 ) ? 'none':'';
+        const isBigScreen = getScreenSize();
+        toggleMenu(isBigScreen)   
     };
+    
     window.onresize = function(){
-        let ww = window.innerWidth;
-        menu.style.display = ( ww > 960 ) ? '':'none';
-        btnBurger.style.display = ( ww > 960 ) ? 'none':'';
+        const isBigScreen = getScreenSize();
+        toggleMenu(isBigScreen)
     };
 });
+
+function getScreenSize() {
+    const windowWidth = window.innerWidth;
+    return windowWidth > 960;
+}
+
+function toggleMenu(screen) {
+    (screen) ? showHtmlElement(menu) : hideHtmlElement(menu);
+    (screen) ? hideHtmlElement(btnBurger) : showHtmlElement(btnBurger);
+}
