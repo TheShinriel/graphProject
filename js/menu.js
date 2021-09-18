292f66b838ffa1750e074ba43fbdc2c8c4c82361
Pierre Henri <payhache@gmail.com>
2021-09-18 14:35:53 +0200

    facto menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,30 +1,45 @@
 import { hideHtmlElement, showHtmlElement } from "./utils/Dom";
 
 window.addEventListener("DOMContentLoaded", () => {
-    const menu = document.getElementById("myNav");
     const btnBurger = document.getElementById("btn-burger");
 
-    btnBurger.addEventListener('click',function(){
-        menu.style.display = (menu.style.display == 'none')? '':'none';
-    });
+    btnBurger.addEventListener('click',function() {
+        const menu = document.getElementById("myNav");
 
-    window.onload = function(){
-        const isBigScreen = getScreenSize();
-        toggleMenu(isBigScreen)   
-    };
-    
-    window.onresize = function(){
-        const isBigScreen = getScreenSize();
-        toggleMenu(isBigScreen)
-    };
+        (menu.classList.contains('hidden')) ? showHtmlElement(menu) : hideHtmlElement(menu);
+    });
 });
+
+window.onload = function() {
+    getStyleMenu();
+};
+
+window.onresize = function() {
+    getStyleMenu()
+};
 
 function getScreenSize() {
     const windowWidth = window.innerWidth;
     return windowWidth > 960;
-}
+};
 
-function toggleMenu(screen) {
-    (screen) ? showHtmlElement(menu) : hideHtmlElement(menu);
-    (screen) ? hideHtmlElement(btnBurger) : showHtmlElement(btnBurger);
+function toggleItem(elementToShow, elementTohide) {
+    showHtmlElement(elementToShow);
+    hideHtmlElement(elementTohide);
+};
+
+function getMenuElements() {
+    const menu = {
+        isBigScreen: getScreenSize(),
+        navButtons: document.getElementById("myNav"),
+        btnBurger: document.getElementById("btn-burger")
+    }
+    return menu;
+};
+
+function getStyleMenu() {
+    const menu = getMenuElements();
+ 
+    (menu.isBigScreen)  ?  toggleItem(menu.navButtons, menu.btnBurger) : toggleItem(menu.btnBurger, menu.navButtons);   
+    
 }
