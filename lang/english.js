4527ca9666e3ac06d26e679e59a3e81d7c97a746
Pierre Henri <payhache@gmail.com>
2022-01-31 15:50:49 +0100

    changement d'une trad

--- lang/english.js
+++ lang/english.js
@@ -1,68 +1,77 @@
 export default {
-    // TITLES
-    "need_dose": "A tool to calculate the dose in mg/kg is available.",
-    "title_page": "Assessment of acetaminophen toxicity using an online dynamic Rumack-Matthew nomogram",
-    "title_main": "je ne sais pas ce que je dois écrire",
-    "title_question": "HopiTox, what is it ?",
-    "title_sub_title": "HopiTox is an online dynamic nomogram based on Rumack equation in order to assess the potential risk for hepatotoxicity after acetaminophen acute overdose. Here are tools to calculate dose in mg/kg or acetaminophen half-life when two samples are withdrawn, and a dynamic nomogram in a user-friendly interface. HopiTox is a tool, not a replacement for medical or biological advice.",
-    "title_warningBadData": "An acetaminophen concentration at more than 4h post ingestion should be obtained. An other sample should be drawn.",
-    "nomogram_title": "Rumack-Matthew dynamic nomogram for acetaminophen overdose",
-    "calc_dose_title": "Calculation of the dose in mg/kg",
-    "title_calc_half_life": "Half life calculation",
-    "contact": "Contact us",
+  // TITLES
+  need_dose: "A tool to calculate the dose in mg/kg is available.",
+  title_page:
+    "Assessment of acetaminophen toxicity using an online dynamic Rumack-Matthew nomogram",
+  title_main: "je ne sais pas ce que je dois écrire",
+  title_question: "HopiTox, what is it ?",
+  title_sub_title:
+    "HopiTox is an online dynamic nomogram based on Rumack equation in order to assess the potential risk for hepatotoxicity after acetaminophen acute overdose. Here are tools to calculate dose in mg/kg or acetaminophen half-life when two samples are withdrawn, and a dynamic nomogram in a user-friendly interface. HopiTox is a tool, not a replacement for medical or biological advice.",
+  title_warningBadData:
+    "Rumack-Matthew nomogram should be used within 4 to 24h after ingestion",
+  nomogram_title: "Rumack-Matthew dynamic nomogram for acetaminophen overdose",
+  calc_dose_title: "Calculation of the dose in mg/kg",
+  title_calc_half_life: "Half life calculation",
+  contact: "Contact us",
 
-    //LABELS
-    "label_first_sampling_time_half_life": "First sampling time :",
-    "label_second_sampling_time_half_life": "Second sampling time :",
-    "label_first_concentration_half_life": "First concentration :",
-    "label_Second_concentration_half_life": "Second concentration :",
-    
-    // BUTTONS
-    "calculate_toxicity_label": "Use of the Rumack-Matthew nomogram in management of acetaminophen overdose is only acceptable when the acuity of ingestion is known to be within 24 hours.",
-    "calculate_half_life": "Half-life calculation",
-    "patient_got_risk_label": "Patient has risk factors for enhanced hepatotoxicity (e.g. starvation or malnutrition, consumption of enzyme inducing drugs, a history of chronic alcohol use and chronic debilitating disease), the 100-line should therefore be used. ",
-    "got_multiple_sampling_date_label": "on vire",
-    "checkboxLabelHalfLife": "Two samples were withdrawn, what is the elimination half-life?",
-    "datePickerLabelFirstSample": "Date and time of the first sample",
-    "datePickerLabelSecondSample": "Date and time of the second sample",
-    "calculate_toxicity_btn": "Risk assessment",
-    "calc_dose_submit_btn": "Calculate the dose in mg/kg",
+  //LABELS
+  label_first_sampling_time_half_life: "First sampling time :",
+  label_second_sampling_time_half_life: "Second sampling time :",
+  label_first_concentration_half_life: "First concentration :",
+  label_Second_concentration_half_life: "Second concentration :",
 
-    // INPUTS
-    "calc_dose_patient_weight_input": "Patient's weight (kg)",
-    "calc_dose_paracetamol_dose_input": "Assumed acetaminophen dose (g)",
-    "calculate_toxicity_ingestion_time": "Time since ingestion (h)",
-    "calculate_toxicity_paracetamol_concentration": "Paracetamolemia (mg/L)",
+  // BUTTONS
+  calculate_toxicity_label:
+    "Use of the Rumack-Matthew nomogram in management of acetaminophen overdose is only acceptable when the acuity of ingestion is known to be within 24 hours.",
+  calculate_half_life: "Half-life calculation",
+  patient_got_risk_label:
+    "Patient has risk factors for enhanced hepatotoxicity (e.g. starvation or malnutrition, consumption of enzyme inducing drugs, a history of chronic alcohol use and chronic debilitating disease), the 100-line should therefore be used. ",
+  got_multiple_sampling_date_label: "on vire",
+  checkboxLabelHalfLife:
+    "Two samples were withdrawn, what is the elimination half-life?",
+  datePickerLabelFirstSample: "Date and time of the first sample",
+  datePickerLabelSecondSample: "Date and time of the second sample",
+  calculate_toxicity_btn: "Risk assessment",
+  calc_dose_submit_btn: "Calculate the dose in mg/kg",
 
-    // RESULTS
-    "toxicity_result_probable": "Concentration at T+ resultToReplace h is associate with probable hepatic toxicity",
-    "toxicity_result_possible": "Concentration at T+ resultToReplace h is associate with possible hepatic toxicity",
-    "toxicity_result_ok": "Concentration at T+ resultToReplace h is associate with low risk of hepatic toxicity",
-    "resultHalfLife": "The half-life is : ",
-    "badCalcul": "Half-life could not be calculated. Please verify the data.",
-    "calc_dose_result": "The ingested dose is resultToReplace mg/kg.",
-    "result_calc_half_life": "The result is resultToReplace h.",
+  // INPUTS
+  calc_dose_patient_weight_input: "Patient's weight (kg)",
+  calc_dose_paracetamol_dose_input: "Assumed acetaminophen dose (g)",
+  calculate_toxicity_ingestion_time: "Time since ingestion (h)",
+  calculate_toxicity_paracetamol_concentration: "Paracetamolemia (mg/L)",
 
-    // GRAPH
-    "graph_title": "Rumack and Matthew nomogram",
-    "graph_toxicity_line": "Probable toxicity line (200-line)",
-    "graph_second_line": "Treatment line (150-line)",
-    "graph_patient_saisi": "Patient's concentration",
-    "graph_riskFactor_line": "100-line",
+  // RESULTS
+  toxicity_result_probable:
+    "Concentration at T+ resultToReplace h is associate with probable hepatic toxicity",
+  toxicity_result_possible:
+    "Concentration at T+ resultToReplace h is associate with possible hepatic toxicity",
+  toxicity_result_ok:
+    "Concentration at T+ resultToReplace h is associate with low risk of hepatic toxicity",
+  resultHalfLife: "The half-life is : ",
+  badCalcul: "Half-life could not be calculated. Please verify the data.",
+  calc_dose_result: "The ingested dose is resultToReplace mg/kg.",
+  result_calc_half_life: "The result is resultToReplace h.",
 
-    // OTHERS 
-    "reference_title": "References : ",
-    "contact_title": "HopiTox, who is it ?",
-    "resp_scientifique": "Scientific officer : Dr Manon Launay, PharmD, PhD",
-    "resp_dev_web": "Developers : Pierre-Henri Laurency, Maxime Rogues, Stanislas Ormières, Laurie Villeneuve",
-    "resp_logo": "Logo design : Nicolas Dubtchak",
+  // GRAPH
+  graph_title: "Rumack and Matthew nomogram",
+  graph_toxicity_line: "Probable toxicity line (200-line)",
+  graph_second_line: "Treatment line (150-line)",
+  graph_patient_saisi: "Patient's concentration",
+  graph_riskFactor_line: "100-line",
 
-    //NAVIGATION
-    "item_first": "Dose",
-    "item_second": "Nomogram",
-    "item_third": "Half-life",
+  // OTHERS
+  reference_title: "References : ",
+  contact_title: "HopiTox, who is it ?",
+  resp_scientifique: "Scientific officer : Dr Manon Launay, PharmD, PhD",
+  resp_dev_web:
+    "Developers : Pierre-Henri Laurency, Maxime Rogues, Stanislas Ormières, Laurie Villeneuve",
+  resp_logo: "Logo design : Nicolas Dubtchak",
 
-    //SLOGAN
-    "slogan":"Only design has changed.",
+  //NAVIGATION
+  item_first: "Dose",
+  item_second: "Nomogram",
+  item_third: "Half-life",
 
-}
+  //SLOGAN
+  slogan: "Only design has changed.",
+};
--- lang/french.js
+++ lang/french.js
@@ -1,67 +1,81 @@
 export default {
-    // TITLES
-    "need_dose": "Il est possible de calculer la dose en mg/kg",
-    "title_page": "Evaluation de la toxicité du paracetamol",
-    "title_main": "Toxicité hépatique du paracétamol",
-    "title_question": "HopiTox, qu'est ce que c'est ?",
-    "title_sub_title": "HopiTox est un outil dynamique reprenant l'équation de Rumack dans une interface intuitive pour évaluer le risque toxique après une prise unique massive de paracetamol. Il permet de réaliser le calcul de la dose ingérée en mg/kg, l'utilisation d'un nomogramme dynamique pour évaluer le risque toxique en fonction de la paracétamolémie, et le calcul de la demi-vie. Il ne se subsitue évidemment pas à l'avis médical ni au dialogue clinico-biologique.",
-    "title_warningBadData": "Attention le nomogramme n'est utilisable que si le prélèvement est réalisé au moins 4 heures après ingestion. Reprélever le patient.",
-    "nomogram_title": "Nomogramme dynamique de Rumack-Matthew",
-    "calc_dose_title": "Calcul de la dose en mg/kg",
-    "title_calc_half_life": "Calcul de la demi-vie",
-    "contact": "Nous contacter",
+  // TITLES
+  need_dose: "Il est possible de calculer la dose en mg/kg",
+  title_page: "Evaluation de la toxicité du paracetamol",
+  title_main: "Toxicité hépatique du paracétamol",
+  title_question: "HopiTox, qu'est ce que c'est ?",
+  title_sub_title:
+    "HopiTox est un outil dynamique reprenant l'équation de Rumack dans une interface intuitive pour évaluer le risque toxique après une prise unique massive de paracetamol. Il permet de réaliser le calcul de la dose ingérée en mg/kg, l'utilisation d'un nomogramme dynamique pour évaluer le risque toxique en fonction de la paracétamolémie, et le calcul de la demi-vie. Il ne se subsitue évidemment pas à l'avis médical ni au dialogue clinico-biologique.",
+  title_warningBadData:
+    "Attention le nomogramme n'est utilisable qu'entre 4 heures et 24h après ingestion.",
+  nomogram_title: "Nomogramme dynamique de Rumack-Matthew",
+  calc_dose_title: "Calcul de la dose en mg/kg",
+  title_calc_half_life: "Calcul de la demi-vie",
+  contact: "Nous contacter",
 
-    //LABELS
-    "label_first_sampling_time_half_life": "Date et heure du premier prélèvement :",
-    "label_second_sampling_time_half_life": "Date et heure du deuxième prélèvement :",
-    "label_first_concentration_half_life": "Première concentration :",
-    "label_Second_concentration_half_life": "Deuxième concentration :",
-    
-    // BUTTONS
-    "calculate_toxicity_label": "L'ingestion a eu lieu en prise unique, il y a moins de 24h, et avec un délai connu et fiable donc le nomogramme est utilisable. La co-administration de composés réduisant le transit (ex: opiacés) peut allonger la phase d'absorption.",
-    "calculate_half_life": "Calculer la demi-vie",
-    "patient_got_risk_label": "Le patient présente des facteurs de risque de d'hépatotoxicité (alcoolisme, cirrhose), le seuil de traitement est donc abaissé (ne cocher cette case que si le patient est concerné).",
-    "got_multiple_sampling_date_label": "Il est possible de faire le même calcul en entrant date et heure d'ingestion ou de prélèvement.",
-    "checkboxLabelHalfLife": "J'ai deux prélèvement et souhaite obtenir la demie vie",
-    "datePickerLabelFirstSample": "Première date",
-    "datePickerLabelSecondSample": "Deuxième date",
-    "calculate_toxicity_btn": "Évaluer le risque",
-    "calc_dose_submit_btn": "Calcul de la dose en mg/kg",
+  //LABELS
+  label_first_sampling_time_half_life: "Date et heure du premier prélèvement :",
+  label_second_sampling_time_half_life:
+    "Date et heure du deuxième prélèvement :",
+  label_first_concentration_half_life: "Première concentration :",
+  label_Second_concentration_half_life: "Deuxième concentration :",
 
-    // INPUTS
-    "calc_dose_patient_weight_input": "Poids du patient (kg)",
-    "calc_dose_paracetamol_dose_input": "Dose supposée (g)",
-    "calculate_toxicity_ingestion_time": "Durée post-ingestion (h)",
-    "calculate_toxicity_paracetamol_concentration": "Paracetamolémie (mg/L)",
+  // BUTTONS
+  calculate_toxicity_label:
+    "L'ingestion a eu lieu en prise unique, il y a moins de 24h, et avec un délai connu et fiable donc le nomogramme est utilisable. La co-administration de composés réduisant le transit (ex: opiacés) peut allonger la phase d'absorption.",
+  calculate_half_life: "Calculer la demi-vie",
+  patient_got_risk_label:
+    "Le patient présente des facteurs de risque de d'hépatotoxicité (alcoolisme, cirrhose), le seuil de traitement est donc abaissé (ne cocher cette case que si le patient est concerné).",
+  got_multiple_sampling_date_label:
+    "Il est possible de faire le même calcul en entrant date et heure d'ingestion ou de prélèvement.",
+  checkboxLabelHalfLife:
+    "J'ai deux prélèvement et souhaite obtenir la demie vie",
+  datePickerLabelFirstSample: "Première date",
+  datePickerLabelSecondSample: "Deuxième date",
+  calculate_toxicity_btn: "Évaluer le risque",
+  calc_dose_submit_btn: "Calcul de la dose en mg/kg",
 
-    // RESULTS
-    "toxicity_result_probable": "Concentration à T+ resultToReplace h associée à un risque important de toxicité.",
-    "toxicity_result_possible": "Concentration à T+ resultToReplace h associée à un possible risque de toxicité.",
-    "toxicity_result_ok": "Concentration à T+ resultToReplace h associée à un risque faible de toxicité.",
-    "resultHalfLife": "La demi vie est de : ",
-    "badCalcul": "Les dates ou les concentrations saisies ne permettent pas le calcul de la demi vie.",
-    "calc_dose_result": "La dose ingérée est donc resultToReplace mg/kg.",
-    "result_calc_half_life": "Le résultat est de resultToReplace h.",
-    
-    //GRAPH
-    "graph_title": "Nomogramme de Rumack et Matthew",
-    "graph_toxicity_line": "Limite inférieure du groupe à haut risque probable (ligne 200)",
-    "graph_second_line": "Ligne de traitement ( Ligne 150, NAC)",
-    "graph_patient_saisi": "Concentrations du patient saisies",
-    "graph_riskFactor_line": "Ligne de traitement (NAC) si facteur de risque (ligne 100)",
+  // INPUTS
+  calc_dose_patient_weight_input: "Poids du patient (kg)",
+  calc_dose_paracetamol_dose_input: "Dose supposée (g)",
+  calculate_toxicity_ingestion_time: "Durée post-ingestion (h)",
+  calculate_toxicity_paracetamol_concentration: "Paracetamolémie (mg/L)",
 
-    // OTHERS
-    "reference_title": "Références : ",
-    "contact_title": "HopiTox, qui est-ce ?",
-    "resp_scientifique": "Responsable scientifique : Dr Manon Launay, PharmD, PhD",
-    "resp_dev_web": "Developpeurs : Pierre-Henri Laurency, Maxime Rogues, Stanislas Ormières, Laurie Villeneuve",
-    "resp_logo": "Création du logo : Nicolas Dubtchak",
+  // RESULTS
+  toxicity_result_probable:
+    "Concentration à T+ resultToReplace h associée à un risque important de toxicité.",
+  toxicity_result_possible:
+    "Concentration à T+ resultToReplace h associée à un possible risque de toxicité.",
+  toxicity_result_ok:
+    "Concentration à T+ resultToReplace h associée à un risque faible de toxicité.",
+  resultHalfLife: "La demi vie est de : ",
+  badCalcul:
+    "Les dates ou les concentrations saisies ne permettent pas le calcul de la demi vie.",
+  calc_dose_result: "La dose ingérée est donc resultToReplace mg/kg.",
+  result_calc_half_life: "Le résultat est de resultToReplace h.",
 
-    //NAVIGATION
-    "item_first": "Dose",
-    "item_second": "Nomogramme",
-    "item_third": "Demi-vie",
+  //GRAPH
+  graph_title: "Nomogramme de Rumack et Matthew",
+  graph_toxicity_line:
+    "Limite inférieure du groupe à haut risque probable (ligne 200)",
+  graph_second_line: "Ligne de traitement ( Ligne 150, NAC)",
+  graph_patient_saisi: "Concentrations du patient saisies",
+  graph_riskFactor_line:
+    "Ligne de traitement (NAC) si facteur de risque (ligne 100)",
 
-    //SLOGAN
-    "slogan":"Rien n'a changé, sauf le design.",
-}
+  // OTHERS
+  reference_title: "Références : ",
+  contact_title: "HopiTox, qui est-ce ?",
+  resp_scientifique: "Responsable scientifique : Dr Manon Launay, PharmD, PhD",
+  resp_dev_web:
+    "Developpeurs : Pierre-Henri Laurency, Maxime Rogues, Stanislas Ormières, Laurie Villeneuve",
+  resp_logo: "Création du logo : Nicolas Dubtchak",
+
+  //NAVIGATION
+  item_first: "Dose",
+  item_second: "Nomogramme",
+  item_third: "Demi-vie",
+
+  //SLOGAN
+  slogan: "Rien n'a changé, sauf le design.",
+};
